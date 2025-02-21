917a7e8f7d372f90e415c1a89d8e1332cbd67070
mayank0404 <mayanksharma040404@gmail.com>
2025-02-21 17:41:38 +0530

    final commit

--- index.js
+++ index.js
@@ -1,25 +1,15 @@
-require("dotenv").config();
 const express=require("express");
-const dbConnect = require("./config");
-
+const cors=require("cors")
 const app=express();
 app.use(express.json());
+app.use(cors());
 
-const authRouter=require("./router/auth");
+const authRouter=require("./src/router/auth");
 app.use("/",authRouter);
 
 
+const PORT = process.env.PORT || 3000;
 
-dbConnect()
-.then(async()=>{
-    console.log("database connected");
-    app.listen(7777,()=>{
-        console.log("server is running on 7777");
-        
-    })
-    
-})
-.catch((err)=>{
-    console.log(err);
-    
-})
+app.listen(PORT, () => {
+  console.log(`Server is running on port ${PORT}`);
+});
--- src/router/auth.js
+++ src/router/auth.js
@@ -1,7 +1,5 @@
 const express = require("express");
-const User = require("../models/user");
-const bcrypt = require("bcrypt");
-const { validateSignupData } = require("../utils/validation");
+
 const authRouter = express.Router();
 
 authRouter.get("/bfhl", async (req, res) => {
